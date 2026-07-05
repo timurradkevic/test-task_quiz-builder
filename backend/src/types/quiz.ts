fdcffc5d892cefc31c89999435c3737ecc0361aa
Tymur <timurradkevic84@mail.com>
2026-07-05 17:24:54 +0300

    feat(backend): add quiz domain types

---
+++ backend/src/types/quiz.ts
@@ -0,0 +1,35 @@
+export enum QuestionType {
+  BOOLEAN = 'BOOLEAN',
+  INPUT = 'INPUT',
+  CHECKBOX = 'CHECKBOX',
+}
+
+export interface Option {
+  id?: number;
+  text: string;
+  isCorrect: boolean;
+}
+
+export interface Question {
+  id?: number;
+  text: string;
+  type: QuestionType;
+  options: Option[];
+}
+
+export interface Quiz {
+  id?: number;
+  title: string;
+  questions: Question[];
+}
+
+export interface QuizListItem {
+  id: number;
+  title: string;
+  questionsCount: number;
+}
+
+export interface CreateQuizDto {
+  title: string;
+  questions: Omit<Question, 'id'>[];
+}
